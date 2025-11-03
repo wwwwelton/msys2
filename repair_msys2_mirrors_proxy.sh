d467f948e9fb7a631eb84a369232554adce6b64f
wwwwelton <av4t@hotmail.com>
2025-11-03 13:03:16 -0300

    refactor: add new urls

--- repair_msys2_mirrors_proxy.sh
+++ repair_msys2_mirrors_proxy.sh
@@ -24,68 +24,241 @@ cp -v ${MIRROR_DIR}/mirrorlist.* "$BACKUP_DIR/" || true
 
 echo "[*] Restoring default official mirrors..."
 cat > "${MIRROR_DIR}/mirrorlist.msys" <<'EOF'
-Server = https://mirror.sjtu.edu.cn/msys2/msys/$arch/
-Server = https://download.nus.edu.sg/mirror/msys2/msys/$arch/
-Server = https://ftp.acc.umu.se/mirror/msys2.org/msys/$arch/
+# See https://www.msys2.org/dev/mirrors
+
+## Primary
 Server = https://mirror.msys2.org/msys/$arch/
-Server = https://mirror.selfnet.de/msys2/msys/$arch/
+Server = https://repo.msys2.org/msys/$arch/
+
+## Tier 1
+Server = https://ftp2.osuosl.org/pub/msys2/msys/$arch/
 Server = https://mirror.yandex.ru/mirrors/msys2/msys/$arch/
+Server = https://mirror.accum.se/mirror/msys2.org/msys/$arch/
+Server = https://ftp.nluug.nl/pub/os/windows/msys2/builds/msys/$arch/
+Server = https://mirror.internet.asn.au/pub/msys2/msys/$arch/
+Server = https://mirror.selfnet.de/msys2/msys/$arch/
 Server = https://mirrors.dotsrc.org/msys2/msys/$arch/
+Server = https://mirrors.bfsu.edu.cn/msys2/msys/$arch/
 Server = https://mirrors.tuna.tsinghua.edu.cn/msys2/msys/$arch/
 Server = https://mirrors.ustc.edu.cn/msys2/msys/$arch/
-Server = https://repo.msys2.org/msys/$arch/
+Server = https://mirror.nju.edu.cn/msys2/msys/$arch/
+Server = https://mirror.clarkson.edu/msys2/msys/$arch/
+Server = https://quantum-mirror.hu/mirrors/pub/msys2/msys/$arch/
+Server = https://mirror.archlinux.tw/MSYS2/msys/$arch/
+Server = https://distrohub.kyiv.ua/msys2/msys/$arch/
+Server = https://mirror.umd.edu/msys2/msys/$arch/
+
+## Tier 2
+Server = https://ftp.cc.uoc.gr/mirrors/msys2/msys/$arch/
+Server = https://mirror.jmu.edu/pub/msys2/msys/$arch/
+Server = https://mirrors.piconets.webwerks.in/msys2-mirror/msys/$arch/
+Server = https://www2.futureware.at/~nickoe/msys2-mirror/msys/$arch/
+Server = https://mirrors.sjtug.sjtu.edu.cn/msys2/msys/$arch/
+Server = https://mirrors.bit.edu.cn/msys2/msys/$arch/
+Server = https://mirrors.aliyun.com/msys2/msys/$arch/
+Server = https://mirror.iscas.ac.cn/msys2/msys/$arch/
+Server = https://mirrors.cloud.tencent.com/msys2/msys/$arch/
+Server = https://download.nus.edu.sg/mirror/msys2/msys/$arch/
+Server = https://repo.extreme-ix.org/msys2/msys/$arch/
+
+## === South America (Brazil, Argentina, Chile) ===
+Server = https://mirror.ufscar.br/msys2/msys/$arch
+Server = https://mirror.ufam.edu.br/msys2/msys/$arch
+Server = https://quantum-mirror.hu/mirrors/pub/msys2/msys/$arch
+Server = https://mirrors.sjtug.sjtu.edu.cn/msys2/msys/$arch
+Server = https://mirror.msys2.org/msys/$arch
+Server = https://mirror.mikrogravitation.org/msys2/msys/$arch
+Server = https://mirrors.tuna.tsinghua.edu.cn/msys2/msys/$arch
+Server = https://repo.msys2.org/msys/$arch
+Server = https://mirror.clarkson.edu/msys2/msys/$arch
+Server = https://ftp.nluug.nl/pub/os/windows/msys2/msys/$arch
+
+## === North America ===
+Server = https://mirror.yandex.ru/mirrors/msys2/msys/$arch
+Server = https://mirror.msys2.org/msys/$arch
+Server = https://mirror.koddos.net/msys2/msys/$arch
+Server = https://mirror.cyberbits.eu/msys2/msys/$arch
+Server = https://mirror.init7.net/msys2/msys/$arch
+
+## === Europe ===
+Server = https://mirror.netcologne.de/msys2/msys/$arch
+Server = https://ftp.acc.umu.se/mirror/msys2.org/msys/$arch
+Server = https://ftp.osuosl.org/pub/msys2/msys/$arch
+Server = https://mirror.selfnet.de/msys2/msys/$arch
+Server = https://ftp.gwdg.de/pub/linux/misc/msys2/msys/$arch
+Server = https://mirrors.dotsrc.org/msys2/msys/$arch
+Server = https://mirror.internet.asn.au/pub/msys2/msys/$arch
+Server = https://mirror.archlinux.tw/MSYS2/msys/$arch
+Server = https://distrohub.kyiv.ua/msys2/msys/$arch
+Server = https://mirror.umd.edu/msys2/msys/$arch
 EOF
 
 cat > "${MIRROR_DIR}/mirrorlist.mingw64" <<'EOF'
-Server = https://mirror.sjtu.edu.cn/msys2/mingw/mingw64/
-Server = https://download.nus.edu.sg/mirror/msys2/mingw/mingw64/
-Server = https://ftp.acc.umu.se/mirror/msys2.org/mingw/mingw64/
-Server = https://mirror.msys2.org/mingw/mingw64/
-Server = https://mirror.selfnet.de/msys2/mingw/mingw64/
-Server = https://mirror.yandex.ru/mirrors/msys2/mingw/mingw64/
-Server = https://mirrors.dotsrc.org/msys2/mingw/mingw64/
-Server = https://mirrors.tuna.tsinghua.edu.cn/msys2/mingw/mingw64/
-Server = https://mirrors.ustc.edu.cn/msys2/mingw/mingw64/
-Server = https://repo.msys2.org/mingw/mingw64/
+# See https://www.msys2.org/dev/mirrors
+
+## Primary
+Server = https://mirror.msys2.org/mingw/x86_64/
+Server = https://repo.msys2.org/mingw/x86_64/
+
+## Tier 1
+Server = https://ftp2.osuosl.org/pub/msys2/mingw/x86_64/
+Server = https://mirror.yandex.ru/mirrors/msys2/mingw/x86_64/
+Server = https://mirror.accum.se/mirror/msys2.org/mingw/x86_64/
+Server = https://ftp.nluug.nl/pub/os/windows/msys2/builds/mingw/x86_64/
+Server = https://mirror.internet.asn.au/pub/msys2/mingw/x86_64/
+Server = https://mirror.selfnet.de/msys2/mingw/x86_64/
+Server = https://mirrors.dotsrc.org/msys2/mingw/x86_64/
+Server = https://mirrors.bfsu.edu.cn/msys2/mingw/x86_64/
+Server = https://mirrors.tuna.tsinghua.edu.cn/msys2/mingw/x86_64/
+Server = https://mirrors.ustc.edu.cn/msys2/mingw/x86_64/
+Server = https://mirror.nju.edu.cn/msys2/mingw/x86_64/
+Server = https://mirror.clarkson.edu/msys2/mingw/x86_64/
+Server = https://quantum-mirror.hu/mirrors/pub/msys2/mingw/x86_64/
+Server = https://mirror.archlinux.tw/MSYS2/mingw/x86_64/
+Server = https://distrohub.kyiv.ua/msys2/mingw/x86_64/
+Server = https://mirror.umd.edu/msys2/mingw/x86_64/
+
+## Tier 2
+Server = https://ftp.cc.uoc.gr/mirrors/msys2/mingw/x86_64/
+Server = https://mirror.jmu.edu/pub/msys2/mingw/x86_64/
+Server = https://mirrors.piconets.webwerks.in/msys2-mirror/mingw/x86_64/
+Server = https://www2.futureware.at/~nickoe/msys2-mirror/mingw/x86_64/
+Server = https://mirrors.sjtug.sjtu.edu.cn/msys2/mingw/x86_64/
+Server = https://mirrors.bit.edu.cn/msys2/mingw/x86_64/
+Server = https://mirrors.aliyun.com/msys2/mingw/x86_64/
+Server = https://mirror.iscas.ac.cn/msys2/mingw/x86_64/
+Server = https://mirrors.cloud.tencent.com/msys2/mingw/x86_64/
+Server = https://download.nus.edu.sg/mirror/msys2/mingw/x86_64/
+Server = https://repo.extreme-ix.org/msys2/mingw/x86_64/
+## === South America (Brazil, Argentina, Chile) ===
+Server = https://mirror.ufscar.br/msys2/mingw/x86_64
+Server = https://mirror.ufam.edu.br/msys2/mingw/x86_64
 EOF
 
 cat > "${MIRROR_DIR}/mirrorlist.mingw32" <<'EOF'
-Server = https://mirror.sjtu.edu.cn/msys2/mingw/mingw32/
-Server = https://download.nus.edu.sg/mirror/msys2/mingw/mingw32/
-Server = https://ftp.acc.umu.se/mirror/msys2.org/mingw/mingw32/
-Server = https://mirror.msys2.org/mingw/mingw32/
-Server = https://mirror.selfnet.de/msys2/mingw/mingw32/
-Server = https://mirror.yandex.ru/mirrors/msys2/mingw/mingw32/
-Server = https://mirrors.dotsrc.org/msys2/mingw/mingw32/
-Server = https://mirrors.tuna.tsinghua.edu.cn/msys2/mingw/mingw32/
-Server = https://mirrors.ustc.edu.cn/msys2/mingw/mingw32/
-Server = https://repo.msys2.org/mingw/mingw32/
+# See https://www.msys2.org/dev/mirrors
+
+## Primary
+Server = https://mirror.msys2.org/mingw/i686/
+Server = https://repo.msys2.org/mingw/i686/
+
+## Tier 1
+Server = https://ftp2.osuosl.org/pub/msys2/mingw/i686/
+Server = https://mirror.yandex.ru/mirrors/msys2/mingw/i686/
+Server = https://mirror.accum.se/mirror/msys2.org/mingw/i686/
+Server = https://ftp.nluug.nl/pub/os/windows/msys2/builds/mingw/i686/
+Server = https://mirror.internet.asn.au/pub/msys2/mingw/i686/
+Server = https://mirror.selfnet.de/msys2/mingw/i686/
+Server = https://mirrors.dotsrc.org/msys2/mingw/i686/
+Server = https://mirrors.bfsu.edu.cn/msys2/mingw/i686/
+Server = https://mirrors.tuna.tsinghua.edu.cn/msys2/mingw/i686/
+Server = https://mirrors.ustc.edu.cn/msys2/mingw/i686/
+Server = https://mirror.nju.edu.cn/msys2/mingw/i686/
+Server = https://mirror.clarkson.edu/msys2/mingw/i686/
+Server = https://quantum-mirror.hu/mirrors/pub/msys2/mingw/i686/
+Server = https://mirror.archlinux.tw/MSYS2/mingw/i686/
+Server = https://distrohub.kyiv.ua/msys2/mingw/i686/
+Server = https://mirror.umd.edu/msys2/mingw/i686/
+
+## Tier 2
+Server = https://ftp.cc.uoc.gr/mirrors/msys2/mingw/i686/
+Server = https://mirror.jmu.edu/pub/msys2/mingw/i686/
+Server = https://mirrors.piconets.webwerks.in/msys2-mirror/mingw/i686/
+Server = https://www2.futureware.at/~nickoe/msys2-mirror/mingw/i686/
+Server = https://mirrors.sjtug.sjtu.edu.cn/msys2/mingw/i686/
+Server = https://mirrors.bit.edu.cn/msys2/mingw/i686/
+Server = https://mirrors.aliyun.com/msys2/mingw/i686/
+Server = https://mirror.iscas.ac.cn/msys2/mingw/i686/
+Server = https://mirrors.cloud.tencent.com/msys2/mingw/i686/
+Server = https://download.nus.edu.sg/mirror/msys2/mingw/i686/
+Server = https://repo.extreme-ix.org/msys2/mingw/i686/
+## === South America (Brazil, Argentina, Chile) ===
+Server = https://mirror.ufscar.br/msys2/mingw/i686
+Server = https://mirror.ufam.edu.br/msys2/mingw/i686
 EOF
 
 cat > "${MIRROR_DIR}/mirrorlist.ucrt64" <<'EOF'
-Server = https://mirror.sjtu.edu.cn/msys2/mingw/ucrt64/
-Server = https://download.nus.edu.sg/mirror/msys2/mingw/ucrt64/
-Server = https://ftp.acc.umu.se/mirror/msys2.org/mingw/ucrt64/
+# See https://www.msys2.org/dev/mirrors
+
+## Primary
 Server = https://mirror.msys2.org/mingw/ucrt64/
-Server = https://mirror.selfnet.de/msys2/mingw/ucrt64/
+Server = https://repo.msys2.org/mingw/ucrt64/
+
+## Tier 1
+Server = https://ftp2.osuosl.org/pub/msys2/mingw/ucrt64/
 Server = https://mirror.yandex.ru/mirrors/msys2/mingw/ucrt64/
+Server = https://mirror.accum.se/mirror/msys2.org/mingw/ucrt64/
+Server = https://ftp.nluug.nl/pub/os/windows/msys2/builds/mingw/ucrt64/
+Server = https://mirror.internet.asn.au/pub/msys2/mingw/ucrt64/
+Server = https://mirror.selfnet.de/msys2/mingw/ucrt64/
 Server = https://mirrors.dotsrc.org/msys2/mingw/ucrt64/
+Server = https://mirrors.bfsu.edu.cn/msys2/mingw/ucrt64/
 Server = https://mirrors.tuna.tsinghua.edu.cn/msys2/mingw/ucrt64/
 Server = https://mirrors.ustc.edu.cn/msys2/mingw/ucrt64/
-Server = https://repo.msys2.org/mingw/ucrt64/
+Server = https://mirror.nju.edu.cn/msys2/mingw/ucrt64/
+Server = https://mirror.clarkson.edu/msys2/mingw/ucrt64/
+Server = https://quantum-mirror.hu/mirrors/pub/msys2/mingw/ucrt64/
+Server = https://mirror.archlinux.tw/MSYS2/mingw/ucrt64/
+Server = https://distrohub.kyiv.ua/msys2/mingw/ucrt64/
+Server = https://mirror.umd.edu/msys2/mingw/ucrt64/
+
+## Tier 2
+Server = https://ftp.cc.uoc.gr/mirrors/msys2/mingw/ucrt64/
+Server = https://mirror.jmu.edu/pub/msys2/mingw/ucrt64/
+Server = https://mirrors.piconets.webwerks.in/msys2-mirror/mingw/ucrt64/
+Server = https://www2.futureware.at/~nickoe/msys2-mirror/mingw/ucrt64/
+Server = https://mirrors.sjtug.sjtu.edu.cn/msys2/mingw/ucrt64/
+Server = https://mirrors.bit.edu.cn/msys2/mingw/ucrt64/
+Server = https://mirrors.aliyun.com/msys2/mingw/ucrt64/
+Server = https://mirror.iscas.ac.cn/msys2/mingw/ucrt64/
+Server = https://mirrors.cloud.tencent.com/msys2/mingw/ucrt64/
+Server = https://download.nus.edu.sg/mirror/msys2/mingw/ucrt64/
+Server = https://repo.extreme-ix.org/msys2/mingw/ucrt64/
+## === South America (Brazil, Argentina, Chile) ===
+Server = https://mirror.ufscar.br/msys2/mingw/ucrt64
+Server = https://mirror.ufam.edu.br/msys2/mingw/ucrt64
 EOF
 
 cat > "${MIRROR_DIR}/mirrorlist.clang64" <<'EOF'
-Server = https://mirror.sjtu.edu.cn/msys2/mingw/clang64/
-Server = https://download.nus.edu.sg/mirror/msys2/mingw/clang64/
-Server = https://ftp.acc.umu.se/mirror/msys2.org/mingw/clang64/
+# See https://www.msys2.org/dev/mirrors
+
+## Primary
 Server = https://mirror.msys2.org/mingw/clang64/
-Server = https://mirror.selfnet.de/msys2/mingw/clang64/
+Server = https://repo.msys2.org/mingw/clang64/
+
+## Tier 1
+Server = https://ftp2.osuosl.org/pub/msys2/mingw/clang64/
 Server = https://mirror.yandex.ru/mirrors/msys2/mingw/clang64/
+Server = https://mirror.accum.se/mirror/msys2.org/mingw/clang64/
+Server = https://ftp.nluug.nl/pub/os/windows/msys2/builds/mingw/clang64/
+Server = https://mirror.internet.asn.au/pub/msys2/mingw/clang64/
+Server = https://mirror.selfnet.de/msys2/mingw/clang64/
 Server = https://mirrors.dotsrc.org/msys2/mingw/clang64/
+Server = https://mirrors.bfsu.edu.cn/msys2/mingw/clang64/
 Server = https://mirrors.tuna.tsinghua.edu.cn/msys2/mingw/clang64/
 Server = https://mirrors.ustc.edu.cn/msys2/mingw/clang64/
-Server = https://repo.msys2.org/mingw/clang64/
+Server = https://mirror.nju.edu.cn/msys2/mingw/clang64/
+Server = https://mirror.clarkson.edu/msys2/mingw/clang64/
+Server = https://quantum-mirror.hu/mirrors/pub/msys2/mingw/clang64/
+Server = https://mirror.archlinux.tw/MSYS2/mingw/clang64/
+Server = https://distrohub.kyiv.ua/msys2/mingw/clang64/
+Server = https://mirror.umd.edu/msys2/mingw/clang64/
+
+## Tier 2
+Server = https://ftp.cc.uoc.gr/mirrors/msys2/mingw/clang64/
+Server = https://mirror.jmu.edu/pub/msys2/mingw/clang64/
+Server = https://mirrors.piconets.webwerks.in/msys2-mirror/mingw/clang64/
+Server = https://www2.futureware.at/~nickoe/msys2-mirror/mingw/clang64/
+Server = https://mirrors.sjtug.sjtu.edu.cn/msys2/mingw/clang64/
+Server = https://mirrors.bit.edu.cn/msys2/mingw/clang64/
+Server = https://mirrors.aliyun.com/msys2/mingw/clang64/
+Server = https://mirror.iscas.ac.cn/msys2/mingw/clang64/
+Server = https://mirrors.cloud.tencent.com/msys2/mingw/clang64/
+Server = https://download.nus.edu.sg/mirror/msys2/mingw/clang64/
+Server = https://repo.extreme-ix.org/msys2/mingw/clang64/
+## === South America (Brazil, Argentina, Chile) ===
+Server = https://mirror.ufscar.br/msys2/mingw/clang64
+Server = https://mirror.ufam.edu.br/msys2/mingw/clang64
 EOF
 
 echo "[*] Forcing HTTP (disabling SSL)..."
